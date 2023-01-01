0a6413c933f3224a54acf160a888107a89258981
Güven Karabulut <56564068+guvenkarabulut@users.noreply.github.com>
2023-01-01 20:38:06 +0300

    Add files via upload

---
+++ lib/formMainPage.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shopping_list/transaction.dart';
+import 'package:uuid/uuid.dart';
+import 'package:flutter_shopping_list/main.dart';
+
+class FormMainPage extends StatelessWidget {
+  const FormMainPage();
+  void _navigateToForm(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => MyApp(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double amount = 0;
+    String title = '';
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Form'),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                  hintText: 'Enter Title',
+                ),
+                validator: (value) => value.isEmpty ? 'Enter Title' : null,
+                onChanged: (value) => value.isEmpty ? null : title = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Amount',
+                  hintText: 'Enter Amount',
+                ),
+                validator: (value) => value.isEmpty ? 'Enter Amount' : null,
+                onChanged: (value) =>
+                    value.isEmpty ? null : amount = double.parse(value),
+              ),
+              OutlinedButton(
+                  child: Text('Add Transaction'),
+                  onPressed: () => {
+                        Transaction(
+                            id: Uuid().v4(),
+                            title: title,
+                            amount: amount,
+                            date: DateTime.now()),
+                        _navigateToForm(context)
+                      },
+                  style: OutlinedButton.styleFrom(
+                    primary: Colors.blue,
+                    backgroundColor: Colors.blue[100],
+                    side: BorderSide(color: Colors.blue, width: 2),
+                  ))
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.keyboard_return),
+          onPressed: () {},
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shopping_list/transaction.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter_shopping_list/formMainPage.dart';
 
 void main() => runApp(MyApp());
+
+void _navigateToForm(BuildContext context) {
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (context) => FormMainPage(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -32,68 +41,70 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART'),
-                elevation: 5,
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                child: Card(
+                  color: Colors.blue,
+                  child: Text('CHART'),
+                  elevation: 5,
+                ),
               ),
-            ),
-            Column(
-              children: transactions.map((tx) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.grey[200],
-                            width: 1,
+              Column(
+                children: transactions.map((tx) {
+                  return Card(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 15,
                           ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${tx.amount}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.blue,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            tx.title,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.grey[200],
+                              width: 1,
                             ),
                           ),
-                          Text(DateFormat.yMMMd().format(tx.date),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            '\$${tx.amount}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              tx.title,
                               style: TextStyle(
-                                color: Colors.grey,
-                              )),
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            ),
-          ]),
-    );
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(DateFormat.yMMMd().format(tx.date),
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                )),
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
+            ]),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => _navigateToForm(context),
+        ));
   }
 }
